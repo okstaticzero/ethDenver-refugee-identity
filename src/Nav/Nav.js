515839bdc6c72b459c2d126be523a82e8b871f71
bud-aminof <bud.aninaminf@cognizant.com>
2018-02-17 12:22:16 -0700

    added a nav bar with title and search bar

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -1,15 +1,30 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import { Avatar, MenuButton, ListItem, Toolbar, KebabMenu } from 'react-md';
+import { Toolbar, TextField, FontIcon } from 'react-md';
 
 import './Nav.css';
 
 const Nav = props => {
     return (
         <div className="nav">
-            <Toolbar
-                title="RefugeID"
-            />
+            
+            <div className="title-wrapper">
+                <Toolbar title="RefugeID" />
+            </div>
+
+            <div className="input-wrapper">
+                <TextField
+                    id="search-with-icon-left"
+                    className="search-input"
+                    label="Search refugee blockchain by id, name or origin.. "
+                    type="search"
+                    leftIcon={<FontIcon>search</FontIcon>}
+                    size={150}
+                    customSize="searchText"
+                    fullWidth={true}
+                    onChange={() => {}}
+                />
+            </div>
         </div>
     );
 };
