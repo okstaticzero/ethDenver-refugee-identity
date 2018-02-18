dec905cdd9bfd791026eccc19b3cc32266405172
Matt Wallace <matthew.wallace@cognizant.com>
2018-02-17 23:06:16 -0700

    transfer func

--- src/util/Uport.js
+++ src/util/Uport.js
@@ -1,35 +1,45 @@
-import { Connect, SimpleSigner, MNID } from 'uport-connect'
+import { Connect, SimpleSigner, MNID } from "uport-connect";
 
-const uport = new Connect('Todo DApp', {
-    clientId: '2oxQ33MUXYP6w3uYAzPyGWpRnZjhTfwUFEy',
-    network: 'rinkeby',
-    signer: SimpleSigner('676b366dd903fe7d09ed6f30771515035ccb2c280abd52b63f43aed308d5c23c')
-})
+const uport = new Connect("Todo DApp", {
+  clientId: "2oxQ33MUXYP6w3uYAzPyGWpRnZjhTfwUFEy",
+  network: "rinkeby",
+  signer: SimpleSigner(
+    "676b366dd903fe7d09ed6f30771515035ccb2c280abd52b63f43aed308d5c23c"
+  )
+});
 
 const initAccount = async () => {
-    const userProfile = await uport.requestCredentials({
-        requested: ['name', 'country', 'avatar'],
-        notifications: true // We want this if we want to recieve credentials
-    })
-    // Do something with user identity
-    const decodedId = MNID.decode(userProfile.address)
-    const specificNetworkAddress = decodedId.address //this is the users account  address
-    console.log('contact: ', userProfile);
-    console.log('specificNetworkAddressL ', specificNetworkAddress);
-    return { specificNetworkAddress, userProfile }
-}
+  const userProfile = await uport.requestCredentials({
+    requested: ["name", "country", "avatar"],
+    notifications: true // We want this if we want to recieve credentials
+  });
+  // Do something with user identity
+  const decodedId = MNID.decode(userProfile.address);
+  const specificNetworkAddress = decodedId.address; //this is the users account  address
+  console.log("contact: ", userProfile);
+  console.log("specificNetworkAddressL ", specificNetworkAddress);
+  return { specificNetworkAddress, userProfile };
+};
 
+const transfer = async () => {
+  const userProfile = await uport.requestCredentials({
+    requested: ["name", "country", "avatar"],
+    notifications: true // We want this if we want to recieve credentials
+  });
+
+  return userProfile;
+};
 
 const initRefugeeAccount = async () => {
-    const userProfile = await uport.requestCredentials({
-        requested: ['name', 'country', 'avatar'],
-        notifications: true // We want this if we want to recieve credentials
-    })
-    //have addresss
-}
+  const userProfile = await uport.requestCredentials({
+    requested: ["name", "country", "avatar"],
+    notifications: true // We want this if we want to recieve credentials
+  });
+  //have addresss
+};
 
-const web3 = uport.getWeb3()
-export { web3, uport, MNID, initAccount }
+const web3 = uport.getWeb3();
+export { web3, uport, MNID, initAccount };
 // Attest specific credentials
 /*
 uport.attestCredentials({
