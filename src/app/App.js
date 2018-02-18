78f10462d42909ee8acf45b5641749318181d4d4
Matt Wallace <matthew.wallace@cognizant.com>
2018-02-17 19:30:21 -0700

    added logo

--- src/app/App.js
+++ src/app/App.js
@@ -1,66 +1,68 @@
-import PropTypes from 'prop-types';
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
-import { Route } from 'react-router';
-import Accounts from '../accounts/Accounts';
-import AddNew from '../addNew/AddNew';
-import './App.css';
-import 'material-design-icons/iconfont/material-icons.css';
+import PropTypes from "prop-types";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
+import { Route } from "react-router";
+import Accounts from "../accounts/Accounts";
+import AddNew from "../addNew/AddNew";
+import "./App.css";
+import "material-design-icons/iconfont/material-icons.css";
 
-import Nav from '../Nav/Nav';
-import { redirectUserToSearch, getAllRefugees, newSearchParams } from './AppActions.js';
+import Nav from "../Nav/Nav";
+import {
+  redirectUserToSearch,
+  getAllRefugees,
+  newSearchParams
+} from "./AppActions.js";
 
-import SearchResults from '../SearchResults/SearchResults';
+import SearchResults from "../SearchResults/SearchResults";
 // import ProfileContainer from '../ProfileContainer';
 
-  // <Route path="/profile/:id" component={ProfileContainer} />
+// <Route path="/profile/:id" component={ProfileContainer} />
 export class App extends Component {
-    constructor(props) {
-      super(props);
-    }
-  
+  constructor(props) {
+    super(props);
+  }
+
   componentDidMount() {
     this.props.getAllRefugees();
   }
 
   render() {
-  
     return (
       <div className="App">
-
         <div className="App-header">
           <Nav
-            newSearchParams={ this.props.newSearchParams }
-            redirectUserToSearch={ this.props.redirectUserToSearch } 
-           />
+            newSearchParams={this.props.newSearchParams}
+            redirectUserToSearch={this.props.redirectUserToSearch}
+          />
         </div>
-        
-          
-        <Route path="/search" component={ SearchResults } /> 
+
+        <Route path="/search" component={SearchResults} />
         <Route exact path="/" component={Accounts} />
         <Route path="/addnew" component={AddNew} />
       </div>
     );
   }
-
 }
 
 App.propTypes = {
   dispatch: PropTypes.func,
   redirectUserToSearch: PropTypes.func,
   getAllRefugees: PropTypes.func,
-  newSearchParams: PropTypes.string,
+  newSearchParams: PropTypes.string
 };
 
 function mapStateToProps(state) {
   return {
-    allRefugees: state.app.allRefugees,
+    allRefugees: state.app.allRefugees
   };
 }
 
-export default withRouter(connect(mapStateToProps, { 
-    redirectUserToSearch, 
+export default withRouter(
+  connect(mapStateToProps, {
+    redirectUserToSearch,
     getAllRefugees,
-    newSearchParams 
-  })(App));
+    newSearchParams
+  })(App)
+);
